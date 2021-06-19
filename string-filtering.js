7d3db7b0290f6a2242d3191cc8779805b243f492
hessium <82901961+hessium@users.noreply.github.com>
2021-06-19 19:50:54 +0300

    Update string-filtering.js

--- string-filtering.js
+++ string-filtering.js
@@ -1,1 +1,18 @@
-# js_project
+  const string = "Привет! Как дела?";
+
+  const voweles = ["у", "е", "ы", "а", "о", "э", "ё", "я", "и"];
+
+  const getVowels = stringToFilter => {
+    let extractedVowels = "";
+
+    for (let i = 0; i < stringToFilter.length; i++) {
+      const currentLetter = stringToFilter[i].toLowerCase();
+
+      if (voweles.includes(currentLetter)) {
+        extractedVowels += currentLetter;
+      }
+    }
+    return extractedVowels;
+  }
+
+  console.log(getVowels(string));
